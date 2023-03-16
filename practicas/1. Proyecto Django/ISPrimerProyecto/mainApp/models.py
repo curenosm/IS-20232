85d07b8b906212210a0e67ab8120b801b9034566
Misael Cureño <curenosm@gmail.com>
2023-03-16 14:43:42 -0600

    Avances en el proyecto y agregando avances de actividad de git branching

--- practicas/1. Proyecto Django/ISPrimerProyecto/mainApp/models.py
+++ practicas/1. Proyecto Django/ISPrimerProyecto/mainApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class  Grupo(models.Model):
+    id_grupo  =  models.AutoField(primary_key=True)
+
+class  Estudiante(models.Model):
+    numCta  =  models.IntegerField(default=0, max_length=9)
+    nombres  =  models.CharField(max_length=200)
+    apellidos  =  models.CharField(max_length=200)
+    edad = models.IntegerField(default=0, max_length=3)
+    # Cada estudiante guarda el grupo en el que está inscrito
+    grupo  =  models.ForeignKey(Grupo, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.apellidos} {self.nombres}, ' + f'No. de cuenta: {self.numCta}, ' + f'Edad: {self.edad}, ' + f'Grupo: {self.grupo.id_grupo}'
---
+++ practicas/1. Proyecto Django/ISPrimerProyecto/mainApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name="mainApp"
+urlpatterns = [
+    path('', views.index, name='inicio')
+]
