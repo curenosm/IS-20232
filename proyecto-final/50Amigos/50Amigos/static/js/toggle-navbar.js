971ded4541d25151bed5363a6bab962f93f01ec1
Misael Cureño <curenosm@gmail.com>
2023-06-01 12:41:42 -0600

    Arreglando puertos Docker

---
+++ proyecto-final/50Amigos/50Amigos/static/js/toggle-navbar.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $("button.navbar-toggler").click();
+        }
+    });
+});
